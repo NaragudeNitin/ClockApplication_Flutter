0bd854fb97a373a42027599d213c717de3318c49
NaragudeNitin <naragudenitin@gmail.com>
2023-01-12 18:43:44 +0530

    implemented digital clock

---
+++ android/app/src/main/kotlin/com/example/clock_application_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clock_application_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:clock_application_flutter/clock_view.dart';
+import 'package:flutter/material.dart';
+
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: const Color(0xFF2D2F41),
+        child: const ClockView(),
+      ),
+    );
+  }
+}
